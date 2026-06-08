5a90feb565fa08e8db3f748a8d1fd033e69174db
miisemaks <miise95mmaks@mail.ru>
2026-06-08 17:11:52 +0900

    user seeder added

---
+++ src/seed.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SeederModule } from './modules/seeder.module';
+import { UserSeeder } from '../db/seeds/user.seeder'; //'db/seeds/user.seeder';
+
+async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(SeederModule);
+
+  try {
+    const seeder = appContext.get(UserSeeder);
+    await seeder.seed();
+  } catch (e) {
+    console.error('Ошибка сидинга', e);
+  } finally {
+    await appContext.close();
+  }
+}
+
+void bootstrap();
